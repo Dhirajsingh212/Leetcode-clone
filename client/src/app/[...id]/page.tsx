632ab5d65916d7839c3d6aa9a78c3374d4c4bb28
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-09-08 19:57:32 +0530

    updated client

--- client/src/app/[...id]/page.tsx
+++ client/src/app/[...id]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { EditorComp } from "@/components/Editor";
 import Header from "@/components/Header";
 import { problemsData } from "@/data";
 import { useParams } from "next/navigation";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 
 export default function Component() {
   const { id } = useParams();
@@ -12,10 +14,9 @@ export default function Component() {
   })[0];
 
   const color = (tag: string) => {
-    console.log(tag);
-    if (tag == "Easy") {
-      return "green-500 ";
-    } else if (tag == "Medium") {
+    if (tag === "Easy") {
+      return "green-500";
+    } else if (tag === "Medium") {
       return "yellow-500";
     }
     return "rose-600";
@@ -23,48 +24,72 @@ export default function Component() {
 
   const tagColor = color(problemById.tag);
 
-  console.log(tagColor);
-
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="grid grid-cols-1 md:grid-cols-[1fr_710px] gap-8 w-full px-4 md:px-10 py-10 ">
-        <div className="space-y-6">
-          <div>
-            <h1 className="text-3xl font-bold">{problemById.title}</h1>
-            <p className="text-muted-foreground mt-2">
-              {problemById.description}
-            </p>
-            <p
-              className={`border px-2 text-${tagColor} w-20 text-center rounded-lg border-${tagColor} my-4`}
-            >
-              {problemById.tag}
-            </p>
-          </div>
-          <div className="space-y-4">
-            <h2 className="text-xl font-semibold">Example</h2>
-            {problemById.examples.map((element: any, index: number) => {
-              return (
-                <div className="bg-muted rounded-md p-4" key={index}>
-                  <p className="text-muted-foreground">
-                    Input: {element.input}
-                    <br />
-                    Output: {element.output}
+      <div className="flex-grow w-full px-4 md:px-8 py-6 md:py-10">
+        <PanelGroup
+          direction="horizontal"
+          className="grid grid-cols-1 md:grid-cols-[1fr_710px] gap-6 w-full h-full"
+        >
+          <Panel defaultSize={40} minSize={20}>
+            <div className="min-w-0 h-[500px] md:h-[600px] overflow-y-scroll no-scrollbar pb-6">
+              <div className="space-y-6">
+                <div>
+                  <h1 className="text-2xl md:text-3xl font-bold">
+                    {problemById.title}
+                  </h1>
+                  <p className="text-sm md:text-base text-muted-foreground mt-2">
+                    {problemById.description}
+                  </p>
+                  <p
+                    className={`border px-3 py-1 inline-block text-${tagColor} w-fit text-center text-sm md:text-base rounded-lg border-${tagColor} my-4`}
+                  >
+                    {problemById.tag}
                   </p>
                 </div>
-              );
-            })}
-          </div>
-          <div className="space-y-4">
-            <h2 className="text-xl font-semibold">Constraints</h2>
-            <ul className="list-disc pl-6 text-muted-foreground">
-              {problemById.constraints.map((element: any, index: number) => {
-                return <li key={index}>{element}</li>;
-              })}
-              <li>{"-5000 < />= Node.val < />= 5000"}</li>
-            </ul>
-          </div>
-        </div>
+                <div className="space-y-4">
+                  <h2 className="text-lg md:text-xl font-semibold">Example</h2>
+                  {problemById.examples.map((element: any, index: number) => (
+                    <div
+                      className="bg-muted rounded-md p-4 text-sm md:text-base"
+                      key={index}
+                    >
+                      <p className="text-muted-foreground">
+                        Input: {element.input}
+                        <br />
+                        Output: {element.output}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+                <div className="space-y-4">
+                  <h2 className="text-lg md:text-xl font-semibold">
+                    Constraints
+                  </h2>
+                  <ul className="list-disc pl-6 text-sm md:text-base text-muted-foreground">
+                    {problemById.constraints.map(
+                      (element: any, index: number) => (
+                        <li key={index}>{element}</li>
+                      )
+                    )}
+                    <li>{"-5000 < />= Node.val < />= 5000"}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </Panel>
+
+          <PanelResizeHandle className="w-2 bg-gray-300 cursor-col-resize" />
+
+          <Panel
+            defaultSize={60}
+            minSize={20}
+            className="min-w-0 overflow-hidden"
+          >
+            <EditorComp />
+          </Panel>
+        </PanelGroup>
       </div>
     </div>
   );
---
+++ client/src/components/Editor.tsx
@@ -0,0 +1,90 @@
+"use client";
+import React, { useRef, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import Editor from "@monaco-editor/react";
+import Spinner from "./Spinner";
+import { Button } from "./ui/button";
+import { useRecoilValue } from "recoil";
+import { userState } from "@/atoms";
+import axios from "axios";
+import { BASE_URL } from "@/app/config";
+import { toast } from "sonner";
+
+export function EditorComp() {
+  const editorRef = useRef(null);
+  const [language, setLanguage] = useState<string>("javascript");
+  const [theme, setTheme] = useState<string>("vs-dark");
+  const userStateValue = useRecoilValue(userState);
+
+  function handleEditorDidMount(editor: any, monaco: any) {
+    editorRef.current = editor;
+  }
+
+  async function showValue() {
+    console.log((editorRef?.current as any).getValue());
+    const code = (editorRef?.current as any).getValue();
+    try {
+      const data = await axios.post(`${BASE_URL}code/submit`, {
+        problemId: 1,
+        userId: userStateValue.userId,
+        code: code,
+        language: language,
+      });
+      toast.success(data.data.message);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div className="flex flex-col bg-slate-950 gap-4 p-4 rounded-lg">
+      <div className="flex flex-row gap-4 justify-between">
+        <div className="flex flex-row gap-4">
+          <Select
+            onValueChange={(e) => {
+              setLanguage(e);
+            }}
+          >
+            <SelectTrigger className="w-[180px] text-white">
+              <SelectValue placeholder="Select language" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="javascript">Javascript</SelectItem>
+              <SelectItem value="python">Python</SelectItem>
+              <SelectItem value="cpp">C++</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select
+            onValueChange={(e) => {
+              setTheme(e);
+            }}
+          >
+            <SelectTrigger className="w-[180px] text-white">
+              <SelectValue placeholder="Select theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">light</SelectItem>
+              <SelectItem value="vs-dark">dark</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <Button onClick={showValue}>Run</Button>
+      </div>
+      <Editor
+        theme={theme}
+        height="90vh"
+        language={language}
+        defaultValue="// some comment"
+        onMount={handleEditorDidMount}
+        loading={<Spinner />}
+      />
+    </div>
+  );
+}
